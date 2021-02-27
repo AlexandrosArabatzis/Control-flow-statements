a1a6f31017491deb4bcc237723efb2bd1a79a556
AlexandrosArabatzis <64361373+AlexandrosArabatzis@users.noreply.github.com>
2021-02-27 12:18:32 +0100

    Update DigitSum.java

--- DigitSum.java
+++ DigitSum.java
@@ -1,24 +1,4 @@
-package arabatzis;
-
-
-//Write a method with the name sumDigits that has one int parameter called number. 
-//
-//If parameter is >= 10 then the method should process the number and return sum of all
-//		
-//digits, otherwise return -1 to indicate an invalid value.
-//
-//The numbers from 0-9 have 1 digit so we dont want to process them, also we dont want
-//
-//to process negative numbers, so also return -1 for negative numbers.
-//
-//For example calling the method sumDigits(125) should return 8 since 1 + 2 + 5 = 8.
-//
-//Calling the method sumDigits(1) should return -1 as per requirements described above.
-//
-//Add some code to the main method to test out the sumDigits method to determine that it is working 
-//
-//correctly for valid and invalid values passed as arguments.
-
+// This is a simple method that takes an integer an returns its cross sum.
 
 public class DigitSum {
 
@@ -27,9 +7,7 @@ public class DigitSum {
 		if (number < 10) {
 			return -1;
 		}
-
 		int sum = 0;
-
 		while (number > 0) {
 			
 			// extract last digit
@@ -39,7 +17,6 @@ public class DigitSum {
 			number = number / 10;
 		}
 		return sum;
-
 	}
 	
 	public static void main (String[] args) {
@@ -49,10 +26,12 @@ public class DigitSum {
 		System.out.println(sumDigits(328974));
 		System.out.println(sumDigits(32));
 		System.out.println(sumDigits(-30));
-		System.out.println(sumDigits(-1));
-		System.out.println(sumDigits(0));
-		System.out.println(sumDigits(9));
-		System.out.println(sumDigits(10));
+		
+	// Output:	
+	// 7
+	// 34
+	// 5
+	// 3
 
 	}
 
